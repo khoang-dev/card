0fdd5efb2f7d4cb30ea3ade1b15dbc86be6102f5
kvd <vodangkhoangute@gmail.com>
2023-12-17 16:12:52 +0700

    update config to deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:khoang-dev/card.git main:gh-pages
+git push -f git@github.com:khoang-dev/khoang-dev.github.io main:gh-pages
 
 cd -
